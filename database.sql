ff20bca62aeb8667a965ec13e1ddc8d2a2e17044
saikatbarua3 <103471441+saikatbarua3@users.noreply.github.com>
2024-11-21 20:26:12 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,3 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    age INTEGER
-);
 
 INSERT INTO users (name, age) VALUES ('John Doe', 30);
 INSERT INTO users (name, age) VALUES ('Jane Smith', 25);
